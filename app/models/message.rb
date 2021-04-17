a9d580b37e41ca32c38e3ca662141805f78adc4f
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-04-18 00:44:02 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  # ルーム名(contentカラム)が存在(presence)している場合作成(true)というバリデーションを設定
+  # 空の場合(true)はDBに保存しない
+  validates :content, presence: true
 end
