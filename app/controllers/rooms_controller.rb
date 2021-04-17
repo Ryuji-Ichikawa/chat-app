b797c78342c5243f1ecb45e902b6aa20f6217792
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-04-17 18:53:37 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,23 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    # 保存が成功したかどうかで処理を分岐しています。
+    # 成功した場合はredirect_toメソッドでルートパスにリダイレクトし、
+    # 失敗した場合はrenderメソッドでrooms/new.html.erbのページを表示
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    # 配列に対しての保存をするためuser_ids: []
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,6 @@ class Room < ApplicationRecord
   # 多対多の関係なので、中間テーブルにもユーザーテーブルにもhas_manyを行う
   has_many :room_users
   has_many :users, through: :room_users
+  # ルーム名(nameカラム)が存在(presence)している場合作成(true)というバリデーションを設定
+  validates :name, presence: true
 end
